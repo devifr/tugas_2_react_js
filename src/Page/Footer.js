1f50214d687421ca8c620814220e603832a3cc87
Devi Firdaus Fauzi <dfedogawa3@gmail.com>
2019-08-06 17:23:52 +0700

    tugas_2_react_js

---
+++ src/Page/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+  render(){
+    return (
+      <div>
+        Tugas 2 React Js
+      </div>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/Page/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  render(){
+    return (
+      <div class="header">
+        Home |
+        Product |
+        Kontak |
+        Tentang Kami
+      </div>
+    )
+  }
+}
+
+export default Header;
